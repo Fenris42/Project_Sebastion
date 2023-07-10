86d02e73bf25181d659fb92686d027a61c8fe33a
Matt Todd <mtodd447@gmail.com>
2023-07-10 12:28:21 -0700

    Added Level
    - Created basic level
    - Reserved space for UI at top
    - Added player with basic up/down movement
    - Added idle and run animations
    - Started character base class

---
+++ Assets/Scripts/Base Classes/BC_Character.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BC_Character : MonoBehaviour
+{
+    //public variables
+    public Animator animator;
+
+    //Stats
+    public int MoveSpeed;
+
+    //private variables
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : BC_Character
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for player inputs
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        //both up and down held at same time
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
+        {
+            //force idle animation
+            animator.SetBool("Running", false);
+        }
+        //move up
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + ((Vector3.up * MoveSpeed) * Time.deltaTime);
+
+            //play animation
+            animator.SetBool("Running", true);
+        }
+        //move down
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + ((Vector3.down * MoveSpeed) * Time.deltaTime);
+
+            //play animation
+            animator.SetBool("Running", true);
+        }
+        //return to idle
+        else
+        {
+            animator.SetBool("Running", false);
+        }
+    }
+}
