d57994ab381ad7bb8097ec823ca34101c737b2d9
Matt Todd <mtodd447@gmail.com>
2023-07-10 10:53:54 -0700

    Main Menu
    - Created main menu at launch
    - Added new game button
    - Added quit game button

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //load game level
+    public void NewGame()
+    {
+        SceneManager.LoadScene("World");
+    }
+
+    //quit game to desktop
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
