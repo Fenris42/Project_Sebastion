e6f0a9418441ec47bda75cc184b7392fd944c76c
Matt Todd <mtodd447@gmail.com>
2023-07-10 15:59:02 -0700

    Misc colliders and UI work
    - Added wall health bar slider
    - Added kill planes around level to catch rogue arrows
    - Added wall colliders
    - Restructured game object hierarchy slightly

---
+++ Assets/Scripts/Stat Bar/StatBar.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StatBar : MonoBehaviour
+{
+    //public variables
+    public Slider slider;
+    public int value;
+    public int max;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sanitize();
+        UpdateBar();
+    }
+
+    //ensure values stay in range
+    private void Sanitize()
+    {
+        if (value < 0)
+        {
+            value = 0;
+        }
+        if (value > max)
+        {
+            value = max;
+        }
+    }
+
+    //set slider to percentage of current and max values
+    private void UpdateBar()
+    {
+        //get bar fill ratio
+        float percentage = (float)value / (float)max;
+
+        //scale health bar fill
+        slider.value = percentage;
+    }
+}
